31647b6303b44c1f348a7a487554511e0dcee590
awselali77 <awselali77@gmail.com>
2020-06-20 10:04:59 +0300

    Done

--- app.js
+++ app.js
@@ -16,18 +16,79 @@ x++
 document.getElementById(id).innerHTML = "X"
   }
  }
- if (document.getElementById(1).innerHTML === "X" && document.getElementById(2).innerHTML === "X" && document.getElementById(3).innerHTML === "X"){
-document.getElementById("player").innerHTML = "Player 1 is the winner" 
-}
+ 
 // when the board is full then (Game Over)
  if ((document.getElementById(1).innerHTML === "X" || document.getElementById(1).innerHTML === "O") && (document.getElementById(2).innerHTML === "X" || document.getElementById(2).innerHTML === "O") && (document.getElementById(3).innerHTML === "X" || document.getElementById(3).innerHTML === "O") && (document.getElementById(4).innerHTML === "X" || document.getElementById(4).innerHTML === "O") && (document.getElementById(5).innerHTML === "X" || document.getElementById(5).innerHTML === "O") && (document.getElementById(6).innerHTML === "X" || document.getElementById(6).innerHTML === "O") && (document.getElementById(7).innerHTML === "X" || document.getElementById(7).innerHTML === "O") && (document.getElementById(8).innerHTML === "X" || document.getElementById(8).innerHTML === "O") && (document.getElementById(9).innerHTML === "X" || document.getElementById(9).innerHTML === "O")){
 document.getElementById("player").innerHTML = "Game Over"
  }
-// document.getElementById(id).innerHTML = "done"
-if (document.getElementById("player").innerHTML = "Game Over"){
-	alert("The board will reset")
-location.reload()
+if (document.getElementById("player").innerHTML === "Game Over"){
+	alert("Game Over")
+}
+
+// putting the rules for the game for X
+if (document.getElementById(1).innerHTML === "X" && document.getElementById(2).innerHTML === "X" && document.getElementById(3).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(4).innerHTML === "X" && document.getElementById(5).innerHTML === "X" && document.getElementById(6).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(7).innerHTML === "X" && document.getElementById(8).innerHTML === "X" && document.getElementById(9).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(1).innerHTML === "X" && document.getElementById(4).innerHTML === "X" && document.getElementById(7).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(2).innerHTML === "X" && document.getElementById(5).innerHTML === "X" && document.getElementById(8).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(3).innerHTML === "X" && document.getElementById(6).innerHTML === "X" && document.getElementById(9).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(1).innerHTML === "X" && document.getElementById(5).innerHTML === "X" && document.getElementById(9).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+if (document.getElementById(3).innerHTML === "X" && document.getElementById(5).innerHTML === "X" && document.getElementById(7).innerHTML === "X"){
+document.getElementById("player").innerHTML = "Player 1 is the winner" 
+}
+// putting the rules for the game for O
+if (document.getElementById(1).innerHTML === "O" && document.getElementById(2).innerHTML === "O" && document.getElementById(3).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(4).innerHTML === "O" && document.getElementById(5).innerHTML === "O" && document.getElementById(6).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(7).innerHTML === "O" && document.getElementById(8).innerHTML === "O" && document.getElementById(9).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(1).innerHTML === "O" && document.getElementById(4).innerHTML === "O" && document.getElementById(7).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(2).innerHTML === "O" && document.getElementById(5).innerHTML === "O" && document.getElementById(8).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(3).innerHTML === "O" && document.getElementById(6).innerHTML === "O" && document.getElementById(9).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(1).innerHTML === "O" && document.getElementById(5).innerHTML === "O" && document.getElementById(9).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
+}
+if (document.getElementById(3).innerHTML === "O" && document.getElementById(5).innerHTML === "O" && document.getElementById(7).innerHTML === "O"){
+document.getElementById("player").innerHTML = "Player 2 is the winner" 
 }
 }
-// putting the rules for the game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
